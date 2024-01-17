21fd790a2a56ce19792f6e79067bb431c9dc3c13
hana6270 <hanana9506@naver.com>
2024-01-17 23:01:25 +0900

    Initial commit

---
+++ src/main/java/com/sample/aws_temp/AwsTempApplication.java
@@ -0,0 +1,13 @@
+package com.sample.aws_temp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTempApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsTempApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sample/aws_temp/AwsTempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.aws_temp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsTempApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
