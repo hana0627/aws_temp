9823e85a852b64b82368babf4d433562c9b4168e
hana6270 <hanana9506@naver.com>
2024-01-17 23:09:59 +0900

    1

---
+++ src/main/java/com/sample/aws_temp/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.sample.aws_temp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "<h4>바보</h4>";
+    }
+}
